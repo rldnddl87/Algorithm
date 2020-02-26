d4448b955c04c11637a761a6f47e3b097bd36c8e
giungSong <rldnddl87@gmail.com>
2020-02-26 17:43:23 +0900

    no message

--- src/crackingTheCodingInterview/Q04_02_A.java
+++ src/crackingTheCodingInterview/Q04_02_A.java
@@ -1,7 +1,6 @@
 package crackingTheCodingInterview;
 
 import crackingTheCodingInterview.CtCILibrary.TreeNode;
-import sun.reflect.generics.tree.Tree;
 
 /**
  * 오름차순으로 정렬된 배열이 있다.
@@ -15,37 +14,22 @@ import sun.reflect.generics.tree.Tree;
 
 public class Q04_02_A {
 
-    public static TreeNode createMinimalTree(int[] num) {
 
-        TreeNode node = new TreeNode(0);
-        for(int i = 0; i < num.length; i++) {
-            sort(node,num[i]);
-        }
-        return node;
+    public static TreeNode createMinimalTree(int[] arr) {
+        return createMinimalTree(arr, 0, arr.length - 1);
     }
 
-    //트리 노드의 깊이가 최소가 된다는 것은 결국 정렬하라는 의미
-    public static void sort(TreeNode node, int n) {
-
-        if(node.data <= n) {
-            if(node.parent == null) {
-                TreeNode newNode = new TreeNode(n);
-
-                node.parent = newNode;
-            }
-
-            //해당 노드보다 값이 크지만 부모보다 작을경우 오른쪽에 추가
-
-            //해당 노드보다 크고
-
-        } else if(node.data > n) {
-            if(node.left == null) {
-                TreeNode newNode = new TreeNode(n);
-                node.left = newNode;
-            }else {
-                sort(node.left, n);
-            }
+    //정렬은 이미 되어 있기 때문에, 순차적으로 트리에 넣어주면 된다.
+    public static TreeNode createMinimalTree(int[] arr, int startIdx, int endIdx) {
+        if( endIdx < startIdx) {
+            return null;
         }
+
+        int mid = (startIdx + endIdx) / 2;
+        TreeNode n = new TreeNode(arr[mid]); // Root 트리에 정렬된 값의 중간값을 넣어준다.
+        n.left = createMinimalTree(arr, startIdx, mid - 1);
+        n.right = createMinimalTree(arr, mid + 1, endIdx);
+        return n;
     }
 
     public static void main(String[] args) {
