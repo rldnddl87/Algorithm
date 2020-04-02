0ac3b8ccbf4a9aae7a76e4c98722ea278798dfc0
giungSong <rldnddl87@gmail.com>
2020-04-02 22:25:51 +0900

    비트 조작 문제 풀이

---
+++ src/leetcode/bitManipulation/n693.java
@@ -0,0 +1,39 @@
+package leetcode.bitManipulation;
+/*
+* 693. Binary Number with Alternating Bits
+* Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+* */
+
+/*
+Input: 5
+        Output: True
+        Explanation:
+        The binary representation of 5 is: 101
+*/
+public class n693 {
+    public boolean hasAlternatingBits(int n) {
+
+        int k;
+
+        while( n > 0) {
+            k = getRightDigit(n);
+            n >>= 1;
+            if( isCommonBit(n, k)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int getRightDigit(int n) {
+        int k = (n & 1) == 1 ? 1 : 0;
+        return k;
+    }
+
+    public static boolean isCommonBit(int n, int k) {
+        int z = getRightDigit(n);
+
+        if((z ^ k) == 0) return true;
+        return false;
+    }
+}
