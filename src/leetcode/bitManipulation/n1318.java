d274027923211a45cfc5aef1bf8582028b451bd2
giungSong <rldnddl87@gmail.com>
2020-04-08 17:35:42 +0900

    no message

--- src/leetcode/bitManipulation/n1318.java
+++ src/leetcode/bitManipulation/n1318.java
@@ -1,4 +1,34 @@
 package leetcode.bitManipulation;
+/*
+1318. Minimum Flips to Make a OR b Equal to c
+Given 3 positives numbers a, b and c. Return the minimum flips required in some bits of a and b to make ( a OR b == c ). (bitwise OR operation).
+Flip operation consists of change any single bit 1 to 0 or change the bit 0 to 1 in their binary representation.
+* */
 
+/*
+Input: a = 2, b = 6, c = 5
+Output: 3
+Explanation: After flips a = 1 , b = 4 , c = 5 such that (a OR b == c)
+*/
 public class n1318 {
+    public int minFlips(int a, int b, int c) {
+        //if c == 0 then a and b also 0
+        //if c == 1 then a 0 b 1 or a 1 b 0
+
+        if( a == 0 && b == 0 && c == 0) return 0;
+
+        int bitA = a & 1;
+        int bitB = b & 1;
+        int bitC = c & 1;
+
+        int count = 0;
+
+        if(bitC == 0) {
+            if(bitA != 0) count++;
+            if(bitB != 0) count++;
+        } else {
+            if(bitA == 0 && bitB == 0) count++;
+        }
+        return count + minFlips(a>>=1, b>>=1, c>>=1);
+    }
 }
---
+++ src/leetcode/bitManipulation/n260.java
@@ -0,0 +1,41 @@
+package leetcode.bitManipulation;
+/*
+260. Single Number III
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice.
+Find the two elements that appear only once.
+*/
+
+/*
+Input:  [1,2,1,3,2,5]
+Output: [3,5]
+* */
+public class n260 {
+    public int[] singleNumber(int[] nums) {
+        int[] ans = {0,0};
+
+        int mask = 0;
+        for(int n : nums) {
+            mask ^= n; // 3 XOR 5의 값이 mask에 저장되고..
+        }
+
+        mask = getRightestBit(mask); //해당 mask의 최우측에 있는 1비트만 가져온다.
+
+        for(int num : nums) {
+            if((num & mask) == 0) { //배열의 모든 수를 mask로 비교하면 서로다른 두 수 3과 5는 if와 else문으로 각각 1개씩 빠지게 되고..
+                ans[0] ^= num; //나머지 두개씩 존재하는 같은 수들은 XOR연산을 통해 0가 된다.
+            } else {
+                ans[1] ^= num; //결국 서로 다른 두 수만 남게 됨
+            }
+        }
+        return ans;
+    }
+
+    public int getRightestBit(int n) {
+        int mask = 1;
+
+        while((mask & n) != mask) {
+            mask <<= 1;
+        }
+        return mask & n;
+    }
+}
