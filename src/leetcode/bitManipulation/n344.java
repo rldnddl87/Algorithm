717a3449e5a51a74cf5ba510d2581b0e4d3a65f0
giungSong <rldnddl87@gmail.com>
2020-04-10 17:39:33 +0900

    no message

---
+++ src/leetcode/bitManipulation/n344.java
@@ -0,0 +1,45 @@
+package leetcode.bitManipulation;
+/*
+344. Reverse String
+Write a function that reverses a string. The input string is given as an array of characters char[].
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+You may assume all the characters consist of printable ascii characters.
+* */
+
+/*
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+* */
+public class n344 {
+
+    public void reverseString(char[] s) {
+        //재귀 풀이
+        recur(s, 0, s.length - 1);
+
+
+        //또 다른 풀이
+        int start = 0;
+        int end = s.length - 1;
+
+        char ch;
+        while(start < end) {
+            ch = s[start];
+            s[start] = s[end];
+            s[end] = ch;
+
+            start++;
+            end--;
+        }
+
+    }
+    public void recur(char[] s, int begin, int end) {
+
+        if(begin >= end) return;
+
+        char ch = s[begin];
+        s[begin++] = s[end];
+        s[end--] = ch;
+
+        recur(s, begin, end );
+    }
+}
---
+++ src/leetcode/bitManipulation/n412.java
@@ -0,0 +1,38 @@
+package leetcode.bitManipulation;
+import java.util.*;
+/*
+412. Fizz Buzz
+Write a program that outputs the string representation of numbers from 1 to n.
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+For numbers which are multiples of both three and five output “FizzBuzz”.
+* */
+public class n412 {
+    public List<String> fizzBuzz(int n) {
+        //3의 배수 Fizz
+        //5의 배수 Buzz
+        //15의 배수 FizzBuzz
+
+        List<String> list = new ArrayList<>();
+
+        for(int i = 1; i <= n; i++) {
+            String str = "";
+
+            if( i % 3 == 0) {
+                str+="Fizz";
+            }
+
+            if( i % 5 == 0) {
+                str+="Buzz";
+            }
+
+            if( str.equals("")) {
+                str += i;
+            }
+
+            list.add(str);
+        }
+
+        return list;
+
+    }
+}
